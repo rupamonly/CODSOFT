c805efd789649ebd30c990949ebfaa6e2a1b07c0
rupamonly <122716395+rupamonly@users.noreply.github.com>
2023-08-30 13:01:43 +0530

    calculator completed

---
+++ Level 1/task 3-calculator/cal.js
@@ -0,0 +1,36 @@
+let Screen = document.getElementById('screen')
+
+function display(num) {
+    Screen.value += num
+}
+
+
+// --------------------function to calculate-----------------------------
+
+function Evaluate() {
+    try {
+        Screen.value = eval(Screen.value)
+    }
+    catch (err) {
+        alert("Invalid")
+    }
+}
+
+// -----------------------functions to clear all and one by one---------------------------
+
+function Clear() {
+    Screen.value = ""
+}
+
+function Del() {
+    Screen.value = Screen.value.slice(0, -1)
+}
+
+
+// -------------function to play audio while clicked-------------------------
+
+function playAudio(audioFileName) {
+    var audio = document.getElementById('audio');
+    audio.src = audioFileName;
+    audio.play();
+}
